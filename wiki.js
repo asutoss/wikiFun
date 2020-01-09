4c38b5383469dc8f4b8512be56d376985736dbf4
AsUToss <ashutoshpal0456@gmail.com>
2020-01-09 19:48:23 +0530

    initial commit

---
+++ wiki.js
@@ -0,0 +1,48 @@
+const request = require('request')
+const express=require('express')
+
+const app=express();
+
+app.get('/info',async(req,res)=>{
+
+
+    //const place='Taj mahal'
+    const place=   encodeURI(req.query.location);
+  
+    if(!req.query.location){
+        return res.send({
+            error:'You must provide address to use the app'
+        })
+    }
+
+
+    const url='https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&generator=search&gsrsearch='+place+'&exlimit=max&exintro&explaintext'//explaintext
+    
+    request({ url: url, json: true }, (error, response) => {
+
+        if(error){
+           return res.send({error:'Unable to connect to internet'})
+        }
+        if(!response.body.query){
+            return res.send("Sorry! Currently no information available.")
+        }
+        //console.log(response.body);
+        //console.log("=================================")
+
+
+        var list = response.body.query.pages;
+
+        var extract = list[Object.keys(list)[0]].extract;
+
+        console.log(extract);
+        res.send(extract);
+    
+    })
+
+
+})
+
+
+app.listen(3000,()=>{
+    console.log(" server is up!")
+})
